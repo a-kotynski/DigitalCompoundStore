f2f2df6965e734153925c1d4bcf98c47f840cb73
A <a.kotynski@hotmail.com>
2023-02-17 13:40:58 +0100

    added connection string

--- VideoGamesStoreInventory/Data/VideoGamesStoreDbContext.cs
+++ VideoGamesStoreInventory/Data/VideoGamesStoreDbContext.cs
@@ -1,8 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using VideoGamesStoreInventory.Entities;
 
 namespace VideoGamesStoreInventory.Data
 {
     public class VideoGamesStoreDbContext : DbContext
     {
+        public VideoGamesStoreDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
     }
 }
