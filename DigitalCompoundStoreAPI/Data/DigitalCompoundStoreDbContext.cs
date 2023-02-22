17aad30a5655613d098e62a0b4d26e31022d4584
A <a.kotynski@hotmail.com>
2023-02-22 11:12:02 +0100

    added contents of DbContext; required further invastigation into enum type of database tables or substituting that with something simpler

--- DigitalCompoundStoreAPI/Data/DigitalCompoundStoreDbContext.cs
+++ DigitalCompoundStoreAPI/Data/DigitalCompoundStoreDbContext.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using DigitalCompoundStoreAPI.Entities;
+using System.Reflection.Emit;
+using System;
 
 namespace DigitalCompoundStoreAPI.Data;
 
@@ -7,6 +9,21 @@ public class DigitalCompoundStoreDbContext : DbContext
 {
     public DigitalCompoundStoreDbContext(DbContextOptions options) : base(options)
     {
+        
     }
     public DbSet<Product> Products { get; set; }
+    public DbSet<Price> Prices { get; set; }
+    public DbSet<PriceUsed> PricesUsed { get; set; }
+    public DbSet<Facility> Facilities { get; set; }
+    public DbSet<Address> Addresses { get; set; }
+    public DbSet<VideoGame> VideoGames { get; set; }
+    public DbSet<FacilityType> FacilityTypes { get; set; } // TODO change enum to something simpler to migrate
+    public DbSet<VideoGamePlatform> VideoGamePlatforms { get; set; } // TODO change enum to something simpler to migrate
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Facility>()
+            .Property(p => p.FacilityType)
+            .HasConversion<string>();
+    }
 }
