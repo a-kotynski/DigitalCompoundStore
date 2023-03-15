73bae6f51aedf99ad56ae5c9dd3be840b483de07
A <a.kotynski@hotmail.com>
2023-03-15 10:21:42 +0100

    another migration, still error "The ConnectionString property has not been initialized."

---
+++ DigitalCompoundStoreAPI/Data/Migrations/20230315091945_CommentedOutEverythingRelatedToEnumProperties.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DigitalCompoundStoreAPI.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CommentedOutEverythingRelatedToEnumProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "VideoGamePlatform",
+                table: "VideoGames");
+
+            migrationBuilder.DropColumn(
+                name: "FacilityType",
+                table: "Facilities");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "VideoGamePlatform",
+                table: "VideoGames",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FacilityType",
+                table: "Facilities",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
